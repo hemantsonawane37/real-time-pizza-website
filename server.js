49cd59a0f6d244fab3f9402912cf66852fc1e43d
hemant-sonawane <hemantsonawanem@gmail.com>
2021-08-20 10:55:02 +0530

    initial

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app     = express();
+const PORT    = process.env.PORT || 3737 ;
+const path    = require("path");
+const viewpath = path.join(__dirname ,'resources','views')
+app.set('views', viewpath)
+app.set('view engine','ejs')
+
+app.get('/',(req,res)=> {
+    res.render("home")
+})
+
+app.listen(PORT,()=> {
+    console.log(`server listening to ${PORT}`)
+})
