002985a6ec2f16dcf93bda193101057118724b0d
hemant-sonawane <hemantsonawanem@gmail.com>
2021-08-20 14:03:40 +0530

    make home page

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./resources/js/app.js":
+/*!*****************************!*\
+  !*** ./resources/js/app.js ***!
+  \*****************************/
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ "./resources/scss/app.scss":
+/*!*********************************!*\
+  !*** ./resources/scss/app.scss ***!
+  \*********************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: expected \"}\".\n   ╷\n40 │ }\n   │  ^\n   ╵\n  resources\\scss\\app.scss 40:2  root stylesheet\n    at processResult (C:\\Real-time pizza\\node_modules\\webpack\\lib\\NormalModule.js:721:19)\n    at C:\\Real-time pizza\\node_modules\\webpack\\lib\\NormalModule.js:827:5\n    at C:\\Real-time pizza\\node_modules\\loader-runner\\lib\\LoaderRunner.js:399:11\n    at C:\\Real-time pizza\\node_modules\\loader-runner\\lib\\LoaderRunner.js:251:18\n    at context.callback (C:\\Real-time pizza\\node_modules\\loader-runner\\lib\\LoaderRunner.js:124:13)\n    at C:\\Real-time pizza\\node_modules\\sass-loader\\dist\\index.js:54:7\n    at Function.call$2 (C:\\Real-time pizza\\node_modules\\sass\\sass.dart.js:93948:16)\n    at _render_closure1.call$2 (C:\\Real-time pizza\\node_modules\\sass\\sass.dart.js:82175:12)\n    at _RootZone.runBinary$3$3 (C:\\Real-time pizza\\node_modules\\sass\\sass.dart.js:27558:18)\n    at _FutureListener.handleError$1 (C:\\Real-time pizza\\node_modules\\sass\\sass.dart.js:26107:19)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	__webpack_modules__["./resources/js/app.js"]();
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./resources/scss/app.scss"]();
+/******/ 	
+/******/ })()
+;
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app     = express();
 const PORT    = process.env.PORT || 3737 ;
 const path    = require("path");
+
+app.use(express.static(path.join(__dirname,'public')));
+
 const viewpath = path.join(__dirname ,'resources','views')
 app.set('views', viewpath)
 app.set('view engine','ejs')
